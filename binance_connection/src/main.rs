97475f23389e0916866c73b8c7b46afeee207ad4
Alby Brown <59367989+BizzQuit7317@users.noreply.github.com>
2025-10-03 11:25:10 +0100

    Update main.rs

--- binance_connection/src/main.rs
+++ binance_connection/src/main.rs
@@ -10,9 +10,21 @@ use serde_json::Value;
 #[tokio::main]
 async fn main() {
     let mut Binance_Client = structs::BinanceClient::new(String::from(""), String::from(""));
-    let response = Binance_Client.send_request::<Value>("fapi", "/fapi/v2/account", Method::GET, None).await;
 
-    println!("{:?}", response);
+    /*
+        Collection all raw binance calls we will need
+        let _wallet = Binance_Client.polling("api", "", Method::GET, None).await;
+    */
+    let future_wallet = Binance_Client.polling("fapi", "/fapi/v2/balance", Method::GET, None).await;
+    let m_wallet = Binance_Client.polling("dapi", "/dapi/v1/balance", Method::GET, None).await;
+    let spot_wallet = Binance_Client.polling("api", "/sapi/v1/capital/config/getall", Method::GET, Some(json!({"type": "SPOT"}))).await;
+    let margin_wallet = Binance_Client.polling("api", "/sapi/v1/margin/account", Method::GET, None).await;
+    let isolated_margin_wallet = Binance_Client.polling("api", "/sapi/v1/margin/isolated/account", Method::GET, None).await;
+    let earn_statking_wallet = Binance_Client.polling("api", "/sapi/v1/staking/position", Method::GET, None).await;
+    let earn_locked_wallet = Binance_Client.polling("api", "/sapi/v1/simple-earn/flexible/position", Method::GET, None).await;
+    let maint_margin = Binance_Client.polling("fapi", "/fapi/v2/account", Method::GET, None).await;
+    let futures_positions = Binance_Client.polling("fapi", "/fapi/v2/positionRisk", Method::GET, None).await;
+    let m_positions = Binance_Client.polling("dapi", "/dapi/v1/positionRisk", Method::GET, None).await;
 
     /*
     let mut MongoClient = ClientStruct::new("Raw_Exchange_Data").await;
