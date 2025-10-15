532b2065916651f46295ecbf2b3cf2cbdf1fc929
Alby Brown <59367989+BizzQuit7317@users.noreply.github.com>
2025-10-15 12:12:37 +0100

    Update main.rs

--- gpg_broker/secret_daemon/src/main.rs
+++ gpg_broker/secret_daemon/src/main.rs
@@ -1,1 +1,10 @@
-a
+use rpassword;
+use secrecy::SecretString;
+
+fn main() {
+    println!("Enter passkey: ");
+    let password = SecretString::new(rpassword::read_password().unwrap().into());
+
+    println!("you entered: {:?}", password);
+
+}
