426938835800ed1408f135b7c8cd629689c4ad21
Alby Brown <59367989+BizzQuit7317@users.noreply.github.com>
2025-10-31 13:54:18 +0000

    Update main.rs

--- gpg_broker/secret_daemon/src/main.rs
+++ gpg_broker/secret_daemon/src/main.rs
@@ -7,6 +7,7 @@ use std::io::BufReader;
 use pgp::crypto;
 use pgp::types::StringToKey;
 use rand::rngs::OsRng;
+use std::io::Read;
 
 fn main() {
     println!("Enter passkey: ");
@@ -70,9 +71,104 @@ fn main() {
                     }
                     Packet::SymEncryptedProtectedData(sepd) => {
                         //println!("Session key -> {:?}\ndata to decrypt -> {:?}, packet -> {:?}", session_key, std::any::type_name_of_val(&sepd), sepd.data());
-                        let data_bytes = sepd.decrypt(&session_key, Some(sym_algo));
+                        //let data_bytes = sepd.decrypt(&session_key, Some(sym_algo));
                         //let data_plain_text = String::from_utf8(data_bytes.unwrap()).expect("[ERR]Invalid UTF-8! ");
-                        println!("data bytess -> {:?}", data_bytes);
+                        //println!("data bytess -> {:?}", data_bytes);
+
+                        //let mut inner_parsser = PacketParser::new(&data_bytes[..]);
+
+                        //while let Some(inner_val) = inner_parsser.next() {
+                        //    match inner_val {
+                        //        Ok(inner_packet) => {
+                        //            println!("inner_packet -> {:?}", inner_packet);
+                        //        }
+                        //        Err(e) => println!("[ERR]No inner packet! {:?}", e),
+                        //    }
+                        //}a
+
+                        match sepd.decrypt(&session_key, Some(sym_algo)) {
+                            Ok(data_bytes) => {
+
+                                println!("[DBG] Successfully decrypted bulk data, now parsing inner packets...");
+
+                                let mut inner_parser = PacketParser::new(&data_bytes[..]);
+
+                                while let Some(inner_val) = inner_parser.next() {
+                                    match inner_val {
+                                        Ok(inner_packet) => {
+                                            match inner_packet {
+                                                Packet::LiteralData(literal_data) => {
+                                                    println!("\n--- 🎉 FOUND PLAIN TEXT DATA (LiteralData) 🎉 ---");
+                                                    let plaintext_bytes = literal_data.data();
+
+                                                    match String::from_utf8(plaintext_bytes.to_vec()) {
+                                                        Ok(plaintext) => {
+                                                            println!("Plain Text:\n{}", plaintext);
+                                                        }
+                                                        Err(_) => {
+                                                            println!("[ERR]Data is not valid UTF-8. Raw bytes: {:?}", plaintext_bytes);
+                                                        }
+                                                    }
+                                                },
+                                                Packet::CompressedData(mut comp_data) => {
+                                                    /*
+                                                    let decompressed_data = comp_data.decompress();
+                                                    println!("Decompressed data -> {:?}", decompressed_data);
+                                                    decompressed_data.fill_buf();
+                                                    println!("Decompressed data filled buffer -> {:?}", decompressed_data);
+                                                    */
+
+                                                    match comp_data.decompress() {
+                                                        Ok(mut decomp_data) => {
+                                                            println!("data decompressed");
+                                                            println!("data -> {:?}", decomp_data);
+
+                                                            let mut decompressed_bytes = Vec::new();
+
+                                                            match decomp_data.read_to_end(&mut decompressed_bytes) {
+                                                                Ok(_) => {
+                                                                    println!("Decoompression sucessfull -> {:?}", decompressed_bytes);
+
+                                                                    let mut deepest_parser = PacketParser::new(&decompressed_bytes[..]);
+
+                                                                    while let Some(deep_val) = deepest_parser.next() {
+                                                                        match deep_val {
+                                                                            Ok(deep_packet) => {
+                                                                                if let Packet::LiteralData(literal_data) = deep_packet {
+                                                                                    let plaintext_bytes = literal_data.data();
+
+                                                                                    match String::from_utf8(plaintext_bytes.to_vec()) {
+                                                                                        Ok(plaintext) => {
+                                                                                            println!("Plain Text:\n{}", plaintext);
+                                                                                        }
+                                                                                        Err(e) => {
+                                                                                            println!("[ERR] Data is not valid UTF-8. Error: {:?}", e);
+                                                                                            println!("Raw bytes: {:?}", plaintext_bytes);
+                                                                                        }
+                                                                                    }
+                                                                                }
+
+                                                                            }
+                                                                            Err(e) => println!("[ERR]Parsing deepest packet! {:?}", e),
+                                                                        }
+                                                                    }
+                                                                }
+                                                                Err(e) => println!("[ERR]Failed to decompress {}", e),
+                                                            }
+
+                                                        }
+                                                        Err(e) => println!("[ERR]No data! {:?}", e),
+                                                    }
+                                                },
+                                                _ => println!("[DBG] Inner packet was: {}", std::any::type_name_of_val(&inner_packet)),
+                                            }
+                                        }
+                                        Err(e) => println!("[ERR] Parsing inner packet! {:?}", e),
+                                    }
+                                }
+                            }
+                            Err(e) => println!("[ERR] Could not decrypt SymEncryptedProtectedData! {:?}", e),
+                        }
 
 
                     }
