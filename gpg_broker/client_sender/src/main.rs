fe1ad5ad2467b50161bfc821187ded051683ad54
Alby Brown <59367989+BizzQuit7317@users.noreply.github.com>
2025-11-26 10:26:29 +0000

    Update main.rs

--- gpg_broker/client_sender/src/main.rs
+++ gpg_broker/client_sender/src/main.rs
@@ -1,14 +1,25 @@
 use tokio::net::UnixStream;
 use tokio::io::{AsyncWriteExt, AsyncReadExt};
 
+pub const AUTH_TOKEN: &str = "SUPER-DUPER-SECRET!!!!";
+
 #[tokio::main]
 async  fn main() {
     let socket_path = "/tmp/uds_learning.sock";
 
     let mut stream = UnixStream::connect(socket_path).await.expect("[ERR]Could not connect!");
 
-    let test_message = "Salam Allahkum brother!";
-
-    stream.write_all(test_message.as_bytes()).await.unwrap();
+    stream.write_all(AUTH_TOKEN.as_bytes()).await.unwrap();
     stream.write_all(b"\n").await.unwrap(); //delimiter
+
+    let mut buf = vec![0u8; 1024];
+    let n = stream.read(&mut buf).await.unwrap();
+    let reply = String::from_utf8_lossy(&buf[..n]);
+
+    if reply.trim() != "AUTH_OK" {
+        println!("Authentication failed: {}", reply);
+        return;
+    }
+
+    println!("Authenticated with server!");
 }
