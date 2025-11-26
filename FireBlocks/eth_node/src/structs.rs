be78b81e79d50f01580e65988fdfce85a222c22e
Alby Brown <59367989+BizzQuit7317@users.noreply.github.com>
2025-11-26 09:55:51 +0000

    Update structs.rs

--- FireBlocks/eth_node/src/structs.rs
+++ FireBlocks/eth_node/src/structs.rs
@@ -1,63 +1,75 @@
-use tokio::net::{UnixListener, UnixStream};
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use std::fs;
-use std::os::unix::fs::PermissionsExt;
-use std::path::Path;
-use anyhow::Result;
+use serde::Deserialize;
+use reqwest::{Client, Error, Method};
+use serde_json::{json, Value};
+use crate::response_structs;
 
-#[tokio::main]
-async fn main() -> Result<()> {
-    let socket_path = "/tmp/uds_learning.sock"; //This file will be created whenthe script is run
-
-    if Path::new(socket_path).exists() {
-        fs::remove_file(socket_path).expect("[ERR]Could not remove file!!! "); //Mkae sure the file doesnt already exist
-    }
-
-    let listener = UnixListener::bind(socket_path).expect("[ERR]Failed to bind to socket!!! ");
-
-    fs::set_permissions(socket_path, fs::Permissions::from_mode(0o700)).expect("[ERR]Failed to set the file permissions!!! ");
-
-    println!("Server listening on {}", socket_path);
-
-    loop {
-        let (stream, _addr) = listener.accept().await.expect("[ERR]Failed to pickup client!!! ");
-        //println!("[DBG]Client connected!!! ");
-
-        tokio::spawn(async move {
-            handle_client(stream).await;
-        });
-
-        //drop(stream)
-    }
-
-    Ok(())
+pub struct EthClient {
+    pub ADDRESS: String,
+    pub BASE_URL: String,
 }
 
-
-async fn handle_client(mut stream: UnixStream) {
-    let mut buf = vec![0u8; 1024];
-
-    loop {
-        match stream.read(&mut buf).await {
-            Ok(0) => {
-                println!("[DBG] Client disconnected");
-                return;
-            }
-            Ok(n) => {
-                let msg = String::from_utf8_lossy(&buf[..n]);
-                println!("[DBG] Received: {}", msg);
-
-                // Send a response back to the client
-                let reply = format!("Server got: {}", msg);
-                if let Err(e) = stream.write_all(reply.as_bytes()).await {
-                    eprintln!("[ERR] Failed to send response: {}", e);
-                    return;
-                }
-            }
-            Err(e) => {
-                eprintln!("[ERR] Reading client! {}", e);
-                return
-            }
+impl EthClient {
+    pub fn new(address: String) -> EthClient {
+        EthClient {
+            ADDRESS: address,
+            BASE_URL: String::from("https://beaconcha.in/api/v1/"),
         }
+    }
+
+    pub async fn send_request<T: for<'de> Deserialize<'de> + Default>(&mut self, endpoint: &str, method: Method, body_data: Option<Value>, address_at_end: bool) -> Result<T, Error> {
+        let mut url = String::new();
+        if address_at_end {
+            url = format!("{}{}{}", self.BASE_URL, endpoint, self.ADDRESS);
+        } else {
+            url = format!("{}{}", self.BASE_URL, endpoint);
+        }
+        let client = Client::new();
+
+        let request_builder = match method {
+            Method::GET => client.get(&url),
+            Method::POST => client.post(&url).json(&body_data.unwrap_or_else(|| json!({}))),
+            Method::PUT => client.put(&url).json(&body_data.unwrap_or_else(|| json!({}))),
+            Method::DELETE => client.delete(&url),
+            _ => {
+                return Ok(T::default())
+            }
+        };
+
+        let response = request_builder.send().await?.json::<T>().await?;
+
+        Ok(response) 
+    }
+
+    pub async fn get_total_balance(&mut self) -> f64 {
+        let mut total_balance: f64 = 0.0;
+        let validators = &self.send_request::<response_structs::ValidatorResponse>("validator/eth1/", Method::GET, None, true).await;
+        match validators {
+            Ok(validator_data) => {
+                match &validator_data.data {
+                    Some(data_outputs) if !data_outputs.is_empty() => {
+                        for output in data_outputs {
+                            let key_delegation_endpoint = format!("validator/{}/deposits", output.publickey);
+                            let key_delegation = &self.send_request::<response_structs::DelegationResponse>(&key_delegation_endpoint, Method::GET, None, false).await;
+                            match key_delegation {
+                                Ok(delegation) => {
+                                    match &delegation.data {
+                                        Some(result_output) if !data_outputs.is_empty() => {
+                                            for result in result_output {
+                                                total_balance += result.amount as f64;
+                                            }
+                                        }
+                                        _ => println!("[WRN] No delegator outputs"),
+                                    }
+                                },
+                                Err(e) => println!("[ERR] No delegations {}", e),
+                            }
+                        }
+                    }
+                    _ => println!("[WRN] No validator outputs"),
+                }
+            },
+            Err(e) => println!("[ERR] No validators {}", e),
+        }
+        total_balance
     }
 }
