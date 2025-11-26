9a3802df50e09b66e3732c173d32bb1a8b327014
Alby Brown <59367989+BizzQuit7317@users.noreply.github.com>
2025-11-26 12:23:00 +0000

    Update struct.rs

--- gpg_broker/client_sender/src/struct.rs
+++ gpg_broker/client_sender/src/struct.rs
@@ -1,7 +1,11 @@
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
 pub struct raw_data {
-    pub data: Vec<key_data>
+    pub data: Vec<key_dat>
 }
 
+#[derive(Debug, Deserialize)]
 pub struct key_data {
     pub id: String,
     pub key: String,
