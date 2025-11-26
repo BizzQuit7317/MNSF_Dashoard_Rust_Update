2ec31de86ff5ab0cf04c90dac551ef4629b8c5a7
Alby Brown <59367989+BizzQuit7317@users.noreply.github.com>
2025-11-26 12:27:30 +0000

    Update main.rs

--- gpg_broker/client_sender/src/main.rs
+++ gpg_broker/client_sender/src/main.rs
@@ -1,5 +1,8 @@
+mod structs;
+
 use tokio::net::UnixStream;
 use tokio::io::{AsyncWriteExt, AsyncReadExt};
+use serde_json;
 
 pub const AUTH_TOKEN: &str = "SUPER-DUPER-SECRET!!!!";
 pub const TMP_EXCHANGE_ID: &str = "binance";
@@ -31,5 +34,11 @@ async  fn main() {
     let n = stream.read(&mut buf).await.unwrap();
     let reply = String::from_utf8_lossy(&buf[..n]);
 
-    println!("Server sent back: {}", reply);
+    match serde_json::from_str(&replay) {
+        Ok(data) => {
+            println!("{}", data);
+        }
+        Err(e) => println!("[ERR]Deserialising string! {:?}", e),
+    }
+
 }
