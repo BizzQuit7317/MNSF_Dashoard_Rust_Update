da61fd9f3386f99f84126cc3a3fe4a778e759273
Alby Brown <59367989+BizzQuit7317@users.noreply.github.com>
2025-11-26 09:52:58 +0000

    Update main.rs

--- gpg_broker/UDS_Learning/src/main.rs
+++ gpg_broker/UDS_Learning/src/main.rs
@@ -1,4 +1,4 @@
-use tokio::net::UnixListener;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use std::fs;
 use std::os::unix::fs::PermissionsExt;
 use std::path::Path;
@@ -14,15 +14,49 @@ async fn main() -> Result<()> {
 
     let listener = UnixListener::bind(socket_path).expect("[ERR]Failed to bind to socket!!! ");
 
-    fs::set_permissions(socket_path, fs::permission::from_mode(0o700)).expect("[ERR]Failed to set the file permissions!!! ");
+    fs::set_permissions(socket_path, fs::Permissions::from_mode(0o700)).expect("[ERR]Failed to set the file permissions!!! ");
 
     println!("Server listening on {}", socket_path);
 
     loop {
         let (stream, _addr) = listener.accept().await.expect("[ERR]Failed to pickup client!!! ");
-        println!("[DBG]Client connected!!! "); //Do something more than just send a connected message and close
-        drop(stream)
+        //println!("[DBG]Client connected!!! ");
+
+        tokio::spawn(async move {
+            handle_client(stream).await;
+        });
+
+        //drop(stream)
     }
 
     Ok(())
+}
+
+
+async fn handle_client(mut stream: UnixStream) {
+    let mut buf = vec![0u8; 1024];
+
+    loop {
+        match stream.read(&mut buf).await {
+            Ok(0) => {
+                println!("[DBG] Client disconnected");
+                return;
+            }
+            Ok(n) => {
+                let msg = String::from_utf8_lossy(&buf[..n]);
+                println!("[DBG] Received: {}", msg);
+
+                // Send a response back to the client
+                let reply = format!("Server got: {}", msg);
+                if let Err(e) = stream.write_all(reply.as_bytes()).await {
+                    eprintln!("[ERR] Failed to send response: {}", e);
+                    return;
+                }
+            }
+            Err(e) => {
+                eprintln!("[ERR] Reading client! {}", e);
+                return
+            }
+        }
+    }
 }
