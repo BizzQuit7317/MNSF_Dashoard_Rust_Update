a027eae4e4549c0739c34e9e60bc35af4570c7b1
Alby Brown <59367989+BizzQuit7317@users.noreply.github.com>
2025-12-05 11:31:15 +0000

    Create main.rs

---
+++ gpg_broker/daemon_client/src/main.rs
@@ -0,0 +1,14 @@
+mod structs;
+
+use tokio::net::UnixStream;
+use tokio::io::{AsyncWriteExt, AsyncReadExt};
+use serde_json;
+
+#[tokio::main]
+async fn main() {
+    println!("[MAIN] starting!!!");
+    let client = structs::Client::new("SUPER-DUPER-SECRET!!!!", "binance");
+    println!("[MAIN] Client created");
+    let _ = client.collectData().await;
+    println!("[MAIN] Finsihed running!!!");
+}
