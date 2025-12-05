17dda0b815c58c583a0f266162b90c641b153c8c
Alby Brown <59367989+BizzQuit7317@users.noreply.github.com>
2025-12-05 12:52:21 +0000

    Update lib.rs

--- gpg_broker/daemon_client/src/lib.rs
+++ gpg_broker/daemon_client/src/lib.rs
@@ -5,9 +5,6 @@ use tokio::io::{AsyncWriteExt, AsyncReadExt};
 use serde_json;
 
 pub async fn run_client() {
-    println!("[MAIN] starting!!!");
     let client = structs::Client::new("SUPER-DUPER-SECRET!!!!", "binance");
-    println!("[MAIN] Client created");
     let _ = client.collectData().await;
-    println!("[MAIN] Finsihed running!!!");
 }
