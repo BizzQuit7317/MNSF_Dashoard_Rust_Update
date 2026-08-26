39a69400f22742172c2cb866fb8e198b883c5c62
Alby Brown <59367989+BizzQuit7317@users.noreply.github.com>
2026-08-26 16:25:34 +0100

    Update main.rs

--- binance_connection/src/main.rs
+++ binance_connection/src/main.rs
@@ -1,37 +1,34 @@
 mod structs;
 
-use mongo_connect::ClientStruct;
-use mongodb::bson::doc;
 use serde_json::json;
 use structs::BinanceClient;
 use reqwest::Method;
 use serde_json::Value;
+use mongodb::{bson::{doc, Document}, Client, Database};
 
 #[tokio::main]
 async fn main() {
-    let key_data = run_client().await;
+    let mut Binance_Client = structs::BinanceClient::new("Ooj4sM3xp2sgDODrd4JvC0BeqZXcQyGLkZpjn5cSqegnIN4wUr0SsJLrFG2bInIy".to_string(), "Kfloz0VcZVsEgNHFWxs8n1gAVJwKvbNaAoFw0c42w1I1TJivN7Urq7A6oPZ7E9Cm".to_string()); //main
+    Binance_Client.get_data().await;
 
-    let mut Binance_Client = structs::BinanceClient::new(key_data.key, key_data.secret);
-    let mut MongoClient = ClientStruct::new("").await;
+    let mut Binance_Client_sub = structs::BinanceClient::new("pbVDqkOk5FAZpBbtIIV4B3oILx4WWZGWNRgUKeAvHbDn7UXFjBWT5eJ92toQIc9c".to_string(), "JSZqTHXOmeyoVekENbae8z2m4p9uX3YINTTgnW96MUrlbMhGg4zfNyhhJN62Ia2o".to_string()); //sub 1
+    Binance_Client_sub.get_data().await;
 
-    /*
-        Collection all raw binance calls we will need
-        let _wallet = Binance_Client.polling("api", "", Method::GET, None).await;
-    */
-    let future_wallet = Binance_Client.polling("fapi", "/fapi/v2/balance", Method::GET, None, &mut  MongoClient, "future_wallet").await;
-    let m_wallet = Binance_Client.polling("dapi", "/dapi/v1/balance", Method::GET, None, &mut  MongoClient, "m_wallet").await;
-    let spot_wallet = Binance_Client.polling("api", "/sapi/v1/capital/config/getall", Method::GET, Some(json!({"type": "SPOT"})), &mut  MongoClient, "spot_wallet").await;
-    let margin_wallet = Binance_Client.polling("api", "/sapi/v1/margin/account", Method::GET, None, &mut  MongoClient, "margin_wallet").await;
-    let isolated_margin_wallet = Binance_Client.polling("api", "/sapi/v1/margin/isolated/account", Method::GET, None, &mut  MongoClient, "isolated_margin_wallet").await;
-    let earn_statking_wallet = Binance_Client.polling("api", "/sapi/v1/staking/position", Method::GET, None, &mut  MongoClient, "earn_staking_wallet").await;
-    let earn_locked_wallet = Binance_Client.polling("api", "/sapi/v1/simple-earn/flexible/position", Method::GET, None, &mut  MongoClient, "earn_locked_wallet").await;
-    let maint_margin = Binance_Client.polling("fapi", "/fapi/v2/account", Method::GET, None, &mut  MongoClient, "maint_margin").await;
-    let futures_positions = Binance_Client.polling("fapi", "/fapi/v2/positionRisk", Method::GET, None, &mut  MongoClient, "future_positions").await;
-    let m_positions = Binance_Client.polling("dapi", "/dapi/v1/positionRisk", Method::GET, None, &mut  MongoClient, "m_positions").await;
+    let mut Binance_Client_sub2 = structs::BinanceClient::new("opk1ON0Dtv0CPu0LTxU6W6cOlCSsL1Lmss6n0uQ8WUiCBCt666SPDbsLsuk8GjGb".to_string(), "GcsDsdKVOm20s2phNxgGkVk1HdemfgcKdbjzdJVK8ltwfDsIjgypxhkskCGTnaTK".to_string()); //sub 2
+    Binance_Client_sub2.get_data().await;
 
-    /*
-    let mut MongoClient = ClientStruct::new("Raw_Exchange_Data").await;
-    let _ = MongoClient.select_collection(String::from("test")).await;
-    let _ = MongoClient.push_document_collection(doc! {"Date":"2025-07-28", "Time":"14-02"}).await;
-    */
+    let mut Binance_Client_sub3 = structs::BinanceClient::new("TrHLFkhph1cSwVw3ZtZFUIJgadR4BLRb6vBIPMm3BWoEjihWZe4ecf4fSSb9j287".to_string(), "lutsan9yMJPL9sBUZgMdJ4x3A4nsYrwQarvRiQoPNqaQtDCdDUHaZwqGc94knlca".to_string()); //sub 3
+    Binance_Client_sub3.get_data().await;
+
+    let mut Binance_Client_sub4 = structs::BinanceClient::new("MwRfATCSA1nbGzjABxBUVs3uKC3QA5ZHrAgLHrZeO03L82maWmR5PIE1QktpEps4".to_string(), "rqLNxevmiLBVLu31eS7iytoDT7P47bX0wyWbspXo70ADqQzLIGJvlTAmKF6AtCtq".to_string()); //sub 4
+    Binance_Client_sub4.get_data().await;
+
+    //Push data into db
+    let client = mongodb::Client::with_uri_str("mongodb://localhost:27017".to_string()).await.unwrap();
+    let db = client.database("TEST_ENV");
+    let test_collection: mongodb::Collection<Document> = db.collection("test_docs_binance");
+
+    let test_doc = doc! { "test": "Success" };
+    let insert_result = test_collection.insert_one(test_doc).await.unwrap();
+    println!("Complete!");
 }
