0bcb1999cad54ab2321f7db2521fcb4df321e2be
Alby Brown <59367989+BizzQuit7317@users.noreply.github.com>
2026-09-03 16:38:15 +0100

    Update main.rs

--- bybit_connnection/src/main.rs
+++ bybit_connnection/src/main.rs
@@ -5,10 +5,10 @@ use serde_json::Value;
 
 #[tokio::main]
 async fn main() {
-    let mut Bybit_Client = structs::BybitClient::new(String::from("SdTNT25LySe50uxH3J"), String::from("nXzOigC0YKDYxRc5i0savmM3VYznWlUF8KPo")); //main
+    let mut Bybit_Client = structs::BybitClient::new(String::from(""), String::from("")); //main
     Bybit_Client.get_data().await;
 
-    let mut Bybit_Client_sub1 = structs::BybitClient::new(String::from("kOyYYhccYGkeugZlw3"), String::from("NbRbjtwaJY0dAq5VEyoINYBvhORiTHRPovjB")); //sub1
+    let mut Bybit_Client_sub1 = structs::BybitClient::new(String::from(""), String::from("")); //sub1
     Bybit_Client_sub1.get_data().await;
 
 }
