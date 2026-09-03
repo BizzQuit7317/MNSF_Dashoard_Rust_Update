25d597992e4d71e1aa876967313739f5640af3bb
Alby Brown <59367989+BizzQuit7317@users.noreply.github.com>
2026-09-03 11:12:32 +0100

    Update main.rs

--- bybit_connnection/src/main.rs
+++ bybit_connnection/src/main.rs
@@ -1,20 +1,14 @@
 mod structs;
 
-use mongo_connect::ClientStruct;
-use mongodb::bson::doc;
 use reqwest::Method;
 use serde_json::Value;
 
 #[tokio::main]
 async fn main() {
-    let Bybit_Client = structs::BybitClient::new(String::from(""), String::from(""));
-    let response = Bybit_Client.send_request::<Value>("/v5/account/wallet-balance", Method::GET, None).await;
+    let mut Bybit_Client = structs::BybitClient::new(String::from("SdTNT25LySe50uxH3J"), String::from("nXzOigC0YKDYxRc5i0savmM3VYznWlUF8KPo")); //main
+    Bybit_Client.get_data().await;
 
-    println!("{:?}", response);
+    let mut Bybit_Client_sub1 = structs::BybitClient::new(String::from("kOyYYhccYGkeugZlw3"), String::from("NbRbjtwaJY0dAq5VEyoINYBvhORiTHRPovjB")); //sub1
+    Bybit_Client_sub1.get_data().await;
 
-    /*
-    let mut MongoClient = ClientStruct::new("Raw_Exchange_Data").await;
-    let _ = MongoClient.select_collection(String::from("test")).await;
-    let _ = MongoClient.push_document_collection(doc! {"Date":"2025-07-28", "Exchange":"bybit"}).await;
-    */
 }
