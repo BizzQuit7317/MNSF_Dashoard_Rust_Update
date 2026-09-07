643abf039f3aed6e475e4628998640671bee6a3c
Alby Brown <59367989+BizzQuit7317@users.noreply.github.com>
2026-09-07 09:49:47 +0100

    Update main.rs

--- binance_connection/src/main.rs
+++ binance_connection/src/main.rs
@@ -8,19 +8,19 @@ use mongodb::{bson::{doc, Document}, Client, Database};
 
 #[tokio::main]
 async fn main() {
-    let mut Binance_Client = structs::BinanceClient::new("Ooj4sM3xp2sgDODrd4JvC0BeqZXcQyGLkZpjn5cSqegnIN4wUr0SsJLrFG2bInIy".to_string(), "Kfloz0VcZVsEgNHFWxs8n1gAVJwKvbNaAoFw0c42w1I1TJivN7Urq7A6oPZ7E9Cm".to_string()); //main
+    let mut Binance_Client = structs::BinanceClient::new("".to_string(), "".to_string()); //main
     Binance_Client.get_data().await;
 
-    let mut Binance_Client_sub = structs::BinanceClient::new("pbVDqkOk5FAZpBbtIIV4B3oILx4WWZGWNRgUKeAvHbDn7UXFjBWT5eJ92toQIc9c".to_string(), "JSZqTHXOmeyoVekENbae8z2m4p9uX3YINTTgnW96MUrlbMhGg4zfNyhhJN62Ia2o".to_string()); //sub 1
+    let mut Binance_Client_sub = structs::BinanceClient::new("".to_string(), "".to_string()); //sub 1
     Binance_Client_sub.get_data().await;
 
-    let mut Binance_Client_sub2 = structs::BinanceClient::new("opk1ON0Dtv0CPu0LTxU6W6cOlCSsL1Lmss6n0uQ8WUiCBCt666SPDbsLsuk8GjGb".to_string(), "GcsDsdKVOm20s2phNxgGkVk1HdemfgcKdbjzdJVK8ltwfDsIjgypxhkskCGTnaTK".to_string()); //sub 2
+    let mut Binance_Client_sub2 = structs::BinanceClient::new("".to_string(), "".to_string()); //sub 2
     Binance_Client_sub2.get_data().await;
 
-    let mut Binance_Client_sub3 = structs::BinanceClient::new("TrHLFkhph1cSwVw3ZtZFUIJgadR4BLRb6vBIPMm3BWoEjihWZe4ecf4fSSb9j287".to_string(), "lutsan9yMJPL9sBUZgMdJ4x3A4nsYrwQarvRiQoPNqaQtDCdDUHaZwqGc94knlca".to_string()); //sub 3
+    let mut Binance_Client_sub3 = structs::BinanceClient::new("".to_string(), "".to_string()); //sub 3
     Binance_Client_sub3.get_data().await;
 
-    let mut Binance_Client_sub4 = structs::BinanceClient::new("MwRfATCSA1nbGzjABxBUVs3uKC3QA5ZHrAgLHrZeO03L82maWmR5PIE1QktpEps4".to_string(), "rqLNxevmiLBVLu31eS7iytoDT7P47bX0wyWbspXo70ADqQzLIGJvlTAmKF6AtCtq".to_string()); //sub 4
+    let mut Binance_Client_sub4 = structs::BinanceClient::new("".to_string(), "".to_string()); //sub 4
     Binance_Client_sub4.get_data().await;
 
     //Push data into db
